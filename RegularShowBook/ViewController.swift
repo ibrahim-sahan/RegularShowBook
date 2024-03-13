beb576835a96e2e2a91dd25fbd719ce5dab6f5ad
ibrahim-sahan <ibrsahan8@gmail.com>
2024-03-14 00:17:42 +0300

    Initial Commit

---
+++ RegularShowBook/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RegularShowBook
+//
+//  Created by İbrahim Şahan on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Mordecai"
+        return cell
+        
+    }
+    
+}
+
---
+++ RegularShowBook/detailsVC.swift
@@ -0,0 +1,27 @@
+//
+//  detailsVC.swift
+//  RegularShowBook
+//
+//  Created by İbrahim Şahan on 14.03.2024.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
